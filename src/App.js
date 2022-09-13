c0b31f249fe9bdf98f92eefb9f38108c17aceb2d
Michael Shanks-Marshall <mikeb.shanksm@gmail.com>
2022-09-13 10:30:05 -0400

    app layout fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Campaign Atlas</h1>
       </header>
+      <div className="App-body">
+        <div className="info-container">Left</div>
+        <div className="map-container">Right</div>
+      </div>
     </div>
   );
 }
