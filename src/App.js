2184c58115259df3c65efd0b52696391872d5d47
Michael Shanks-Marshall <mikeb.shanksm@gmail.com>
2022-10-28 11:35:20 -0400

    searchbar added, material-ui savestate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-import MapContainer from './components/MapContainer';
+import SearchBar from './components/SearchBar';
 import InfoContainer from './components/InfoContainer';
+import MapContainer from './components/MapContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchBar placeholder={'Search'}/>
         <h1>Welcome to Campaign Atlas</h1>
+        <div className='viewPicker'>
+
+        </div>
       </header>
       <div className="App-body">
         <InfoContainer />
         <MapContainer />
+        {/* <TimelineContainer />
+        <ConspiracyContainer /> */}
       </div>
     </div>
   );
